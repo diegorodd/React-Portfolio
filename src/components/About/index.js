a374df44b2e8061e1ed6e58ecec2f16ad4b55b79
diegorodd <dieegoa@icloud.com>
2022-09-28 01:05:43 -0400

    almosted finshing the components file

---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section className='about-section'>
+            <div className='flex-row'>
+                <h2 className='section-title primary-border'>
+                    About Me
+                </h2>
+            </div>
+            <article className='about-me-info'>
+                <div className='headshot'>
+                </div>
+                <div className='about-text'>
+                    <p>
+                        About to finish my school and hopfully get the job i desire and be better and life and help my family.
+                        <br />
+                        </p>
+                </div>
+            </article>
+        </section>
+    );
+}
+
+export default About;
